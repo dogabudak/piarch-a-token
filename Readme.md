a395f46e3281f3d120bf248fc7392896a6d90a13
doga budak <dogabudak@dogas-MacBook-Air.local>
2017-11-28 01:53:51 +0100

    charm

---
+++ Readme.md
@@ -0,0 +1,1 @@
+curl -H "authorize:korburak 123456" "127.0.0.1:3012"
--- index.js
+++ index.js
@@ -1,96 +1,58 @@
 /**
- * Created by doga on 22/10/2016.
- */
+* Created by doga on 22/10/2016.
+*/
 
-var logger = require('./lib/logger')(),
-    connect = require('connect'),
-    CORS = require('cors'),
-    checkUserFromDatabase = require('./lib/validation-service.js').checkUserFromDatabase,
-    isBlackListed = require('./lib/blacklist.js').IsBlackListed,
-    isIpBlackListed = require('./lib/blacklist.js').isBlacklistIp,
-    config = require('./resources/config.js');
+const logger = require('./lib/logger')(),
+checkUserFromDatabase = require('./lib/validation-service.js').checkUserFromDatabase,
+isBlackListed = require('./lib/blacklist.js').IsBlackListed,
+isIpBlackListed = require('./lib/blacklist.js').isBlacklistIp,
+config = require('./resources/config.js');
 
-var app = connect();
+const Koa = require('koa');
+const koaApp = new Koa();
+//TODO implement koa-router for adress
+koaApp.use(async (ctx, next) => {
 
-app.use('*', CORS())
-    .use(config.server.path, function (req, res) {
-        if (req.method === 'GET') {
-            tokenReturn(req, res);
-        }
-    });
+  if (ctx.request.header.authorize == undefined) ctx.throw(400);
+
+  let authorizeHeader = ctx.request.header.authorize.split(" ");
+  if (authorizeHeader.length === 2) {
+    var method = authorizeHeader[0];
+    var encoded_user_pass = authorizeHeader[1];
+  } else {
+    ctx.throw(400);
+  };
+
+  let user_info = encoded_user_pass.split(":");
+  if (user_info.length !== 2) ctx.throw(401);
+
+  let user = user_info[0],
+  password = user_info[1];
+  //TODO client ip not implemented
+  let clientIp = (ctx.request.ip).toString();
+  if (user === "" || password === "") ctx.throw(401);
+
+  let reqObj = {"method": method,"credentials": {uid: user, "password": password}};
+  let token;
+
+  let answerIP = await isIpBlackListed(clientIp)
+
+  if (answerIP === true) ctx.throw()
+
+  let resultRedis = await isBlackListed(reqObj.credentials.uid)
+
+  if (resultRedis === true) ctx.throw()
+
+  let tokenObj = await checkUserFromDatabase(reqObj, clientIp, resultRedis, answerIP);
+
+  token = tokenObj;
+  ctx.status = 200
+  ctx.body = JSON.stringify(token);
+  await next();
 
 
-app.listen(config.server.port, function () {
-    console.log("Listening on " + config.server.port);
+
+
 });
-
-
-function tokenReturn(req, res) {
-    if (req.headers.authorization !== undefined) {
-        req.headers.authorize = req.headers.authorization;
-    }
-    if (req.headers.authorize == undefined) {
-        res.statusCode = 400;
-        res.end();
-        return;
-    }
-    console.log(req.headers)
-    if (!/(^\S+\s{1}\S+$)/.test(req.headers.authorize)) {
-        res.statusCode = 400;
-        res.end();
-        return;
-    }
-    var authorizeHeader = req.headers.authorize.split(" ");
-    // TODO jwt is currently supported but will be deprecated
-    if (authorizeHeader.length === 2) {
-        var method = authorizeHeader[0];
-        var encoded_user_pass = authorizeHeader[1];
-    } else {
-        res.statusCode = 400;
-        res.end();
-        return;
-    }
-    ;
-    var user_info = encoded_user_pass.split(":");
-    if (user_info.length !== 2) {
-        res.statusCode = 401;
-        res.end();
-        return;
-    }
-
-    var user = user_info[0],
-        password = user_info[1];
-    var clientIp = (req.connection.remoteAddress).toString();
-    if (user === "" || password === "") {
-        res.statusCode = 401;
-        res.end();
-        return;
-    }
-    var reqObj = {
-        "method": method,
-        "credentials": {uid: user, "password": password}
-    };
-    var token;
-    isIpBlackListed(clientIp, function (answer) {
-        if (answer === true) {
-            res.end();
-            return;
-
-        }
-        isBlackListed(reqObj.credentials.uid, function (result) {
-            if (result === true) {
-                res.end();
-                return;
-            }
-            checkUserFromDatabase(reqObj, clientIp, result, answer, function (err, result) {
-                token = result;
-                console.log(token);
-                res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
-                res.write(JSON.stringify(result));
-                res.end();
-            });
-        });
-
-    })
-}
-
+//TODO implement from config
+koaApp.listen(3012);
