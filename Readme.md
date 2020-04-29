bfdf6fa2026de985d9df1050839387f215beadbb
doga <doga.budak@check24.de>
2020-04-29 17:07:35 +0200

    token mothod correctened

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
-curl -H "authorize:korburak 123456" "127.0.0.1:3012"
+curl -H "authorize:Basic dogabudak:123456" "127.0.0.1:3092"
+
--- index.js
+++ index.js
@@ -2,18 +2,17 @@
 * Created by doga on 22/10/2016.
 */
 
-const logger = require('./lib/logger')(),
-checkUserFromDatabase = require('./lib/validation-service.js').checkUserFromDatabase,
+const checkUserFromDatabase = require('./lib/validation-service.js').checkUserFromDatabase,
 isBlackListed = require('./lib/blacklist.js').IsBlackListed,
-isIpBlackListed = require('./lib/blacklist.js').isBlacklistIp,
-config = require('./resources/config.js');
+isIpBlackListed = require('./lib/blacklist.js').isBlacklistIp;
 
 const Koa = require('koa');
 const koaApp = new Koa();
-//TODO implement koa-router for adress
-koaApp.use(async (ctx, next) => {
 
-  if (ctx.request.header.authorize == undefined) ctx.throw(400);
+//TODO implement koa-router for adress
+
+koaApp.use(async (ctx, next) => {
+  if (ctx.request.header.authorize === undefined) ctx.throw(400);
 
   let authorizeHeader = ctx.request.header.authorize.split(" ");
   if (authorizeHeader.length === 2) {
@@ -21,14 +20,16 @@ koaApp.use(async (ctx, next) => {
     var encoded_user_pass = authorizeHeader[1];
   } else {
     ctx.throw(400);
-  };
+  }
 
   let user_info = encoded_user_pass.split(":");
   if (user_info.length !== 2) ctx.throw(401);
 
-  let user = user_info[0],
-  password = user_info[1];
+  let user = user_info[0].toLowerCase(),
+  password = user_info[1].toLowerCase();
+
   //TODO client ip not implemented
+
   let clientIp = (ctx.request.ip).toString();
   if (user === "" || password === "") ctx.throw(401);
 
@@ -43,12 +44,10 @@ koaApp.use(async (ctx, next) => {
 
   if (resultRedis === true) ctx.throw()
 
-  let tokenObj = await checkUserFromDatabase(reqObj, clientIp, resultRedis, answerIP);
 //TODO implement stream to body
-  token = tokenObj;
+  token = await checkUserFromDatabase(reqObj, clientIp, resultRedis, answerIP);
   ctx.status = 200
   ctx.body = JSON.stringify(token);
-
 });
 //TODO implement from config
-koaApp.listen(3012);
+koaApp.listen(3092);
